ee408cc9f2605958427ff850e045bafa5a21b607
Creanciel <creanciel@gmail.com>
2025-12-01 23:01:42 +0900

    Add: init sample

---
+++ README.md
@@ -0,0 +1,16 @@
+# ZennRubyOugai
+
+Ruby の gem Ougai
+
+## Install & Run
+
+```sh
+cd apps/sample
+
+# bundle init
+# bundle add logger
+# bundle add ougai
+bundle install
+
+bundle exec ruby main.rb
+```
---
+++ apps/sample/main.rb
@@ -0,0 +1,29 @@
+require 'logger'
+require 'json'
+require 'ougai'
+
+logger = Ougai::Logger.new(STDOUT)
+
+logger::error('エラー')
+
+
+begin
+  JSON.parse('{')
+rescue => e
+  logger::error('An error occured', e);
+end
+
+begin
+  raise StandardError.new('エラー')
+rescue => e
+  logger::error('An error occured', e, {
+    name: 'rintaro',
+    book: 'vita sexualis',
+  })
+
+  logger::error(
+    'An error occured',
+    e, name: 'rintaro',
+    book: 'vita sexualis'
+  )
+end
