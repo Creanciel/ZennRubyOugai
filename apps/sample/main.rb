929fe53bb54549d2f620c8e41425825e7402dec4
Creanciel <creanciel@gmail.com>
2025-12-01 23:11:24 +0900

    Fix: method to call method by .

--- apps/sample/main.rb
+++ apps/sample/main.rb
@@ -4,24 +4,24 @@ require 'ougai'
 
 logger = Ougai::Logger.new(STDOUT)
 
-logger::error('エラー')
+logger.error('エラー')
 
 
 begin
   JSON.parse('{')
 rescue => e
-  logger::error('An error occured', e);
+  logger.error('An error occured', e);
 end
 
 begin
   raise StandardError.new('エラー')
 rescue => e
-  logger::error('An error occured', e, {
+  logger.error('An error occured', e, {
     name: 'rintaro',
     book: 'vita sexualis',
   })
 
-  logger::error(
+  logger.error(
     'An error occured',
     e, name: 'rintaro',
     book: 'vita sexualis'
